7fa643ba685400104d5ab65ef9899b9558091564
Omar Hussein <omarelwy@hotmail.com>
2022-06-16 21:15:37 +0200

    ✨ Add different prod, dev, and test envs

--- server/config/index.ts
+++ server/config/index.ts
@@ -2,6 +2,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+type NodeEnv = "production" | "development" | "test";
+
+const env: NodeEnv =
+  (process.env.NODE_ENV as NodeEnv | undefined) || "development";
+
+const isProd = env === "production";
+const isDev = env === "development";
+const isTest = env === "test";
+
+let dbLink: string | undefined;
+
+if (isProd) dbLink = process.env.DB_URI;
+else if (isTest) dbLink = process.env.DB_URI_TEST;
+
+dbLink ||= process.env.DB_URI_DEV;
+
+if (!dbLink) throw new Error("You have to provide the db uri");
+
 export const config = {
-  dbLink: process.env.DB_URI,
+  isProd,
+  isDev,
+  isTest,
+  dbLink,
 };
