ab9619a2bf56f3c108a79fa6f7cf664765a4d7b8
Omar Eloui <contact@omareloui.com>
2023-09-15 17:23:02 +0300

    chore(style): remove animation on search

--- components/LinksList.vue
+++ components/LinksList.vue
@@ -3,9 +3,11 @@ const { preview, refresh } = useLinksStore();
 </script>
 
 <template>
-  <TransitionGroup name="links-preview" class="list" tag="div">
+  <!-- <TransitionGroup name="links-preview" class="list" tag="div"> -->
+  <div class="list">
     <LinkPreview v-for="link in preview.links" :key="link.slug" :link="link" @refresh-list="refresh"></LinkPreview>
-  </TransitionGroup>
+  </div>
+  <!-- </TransitionGroup> -->
 </template>
 
 <style lang="scss">
